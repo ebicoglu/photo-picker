18b5b365217ea3bf12c018fc01f6004615e9a59f
Alper Ebiçoğlu <9526587+ebicoglu@users.noreply.github.com>
2025-02-05 21:19:49 +0300

    add program.cs and readme

--- PhotoPicker/Program.cs
+++ PhotoPicker/Program.cs
@@ -2,20 +2,75 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-
-
+//////// BUILD THE HOST
 var builder = Host.CreateApplicationBuilder();
-
-builder.Services.AddChatClient(new OllamaChatClient(
-    endpoint: new Uri("http://localhost:11434"),
-    modelId: "llama3.2")
-).Build();
+builder
+    .Services
+    .AddChatClient(new OllamaChatClient(
+        endpoint: new Uri("http://localhost:11434"),
+        modelId: "llava"))
+    .Build();
 
 var app = builder.Build();
+
+//////// CREATE THE CHAT CLIENT
 var chatClient = app.Services.GetRequiredService<IChatClient>();
 
-var chatCompletion = await chatClient.CompleteAsync("How can you help me?");
-Console.WriteLine(chatCompletion.Message.Text);
+//////// CREATE MESSAGE
+var messages = new List<ChatMessage>
+{
+    //new ChatMessage {
+    //    Role = ChatRole.System,
+    //    Text = "You are an agent that analyzes images for social platforms. "
+    //}
+};
 
 
-Console.ReadKey();
+//messages.Add(new ChatMessage
+//{
+//    Role = ChatRole.User,
+//    Text = "Describe these photos: \n" +
+//           "https://raw.githubusercontent.com/ebicoglu/photo-picker/refs/heads/main/PhotoPicker/photos/photo-1.jpg, \n" +
+//           "https://raw.githubusercontent.com/ebicoglu/photo-picker/refs/heads/main/PhotoPicker/photos/photo-2.jpg, \n" +
+//           "https://raw.githubusercontent.com/ebicoglu/photo-picker/refs/heads/main/PhotoPicker/photos/photo-3.jpg, \n" +
+//           "https://raw.githubusercontent.com/ebicoglu/photo-picker/refs/heads/main/PhotoPicker/photos/photo-4.jpg"
+//});
+
+ 
+
+//////// ADD IMAGES TO THE MESSAGE
+var contents = new List<AIContent>();
+const string directory = @"D:\github\alper\photo-picker\PhotoPicker\photos";
+var files = Directory.GetFiles(directory, "*.jpg");
+for (var i = 0; i < files.Length; i++)
+{
+    var file = files[i];
+    messages.Add(new ChatMessage
+    {
+        Role = ChatRole.User,
+        Text = "Describe this photo",
+        Contents = new List<AIContent> { new ImageContent(File.ReadAllBytes(file), "image/jpeg") }
+    });
+}
+
+//////// ASK TO AI
+Console.WriteLine(string.Join("\n", messages.Select(x => x.Text).Where(x => x != null)) + "\n\r");
+var chatResponse = await chatClient.CompleteAsync(messages);
+messages.Add(chatResponse.Message);
+
+Console.WriteLine(chatResponse.Message.Text);
+
+
+Console.Write("You > ");
+var input = Console.ReadLine();
+
+while (input != "exit")
+{
+    messages.Add(new ChatMessage { Role = ChatRole.User, Text = input });
+    chatResponse = await chatClient.CompleteAsync(messages);
+    messages.Add(chatResponse.Message);
+    Console.WriteLine("\nAI > " + chatResponse.Message.Text);
+    Console.Write("\nYou > ");
+    input = Console.ReadLine();
+}
+
---
+++ readme.md
@@ -0,0 +1,27 @@
+1. Download https://ollama.com/ and install.
+
+2. Download language model. You can see all models at https://ollama.com/search
+
+```
+cd %LocalAppData%\Programs\Ollama
+ollama pull deepseek-r1
+```
+
+* `deepseek-r1`: 4.7 GB
+* `llama3.2`: 2.0 GB
+* `llava`: 4.7 GB *(for image vision)*
+
+3. Check if it's loaded at http://localhost:11434/api/tags or 
+
+```
+ollama list
+```
+
+
+4. Start Ollama server
+
+```
+cd %LocalAppData%\Programs\Ollama
+ollama serve
+```
+
