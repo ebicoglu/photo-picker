b96cef406109925be896edb0366ce82111cf5f24
Alper Ebiçoğlu <9526587+ebicoglu@users.noreply.github.com>
2025-02-05 17:01:00 +0300

    integrate AI

--- PhotoPicker/Program.cs
+++ PhotoPicker/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services.AddChatClient(new OllamaChatClient(
+    endpoint: new Uri("http://localhost:11434"),
+    modelId: "llama3.2")
+).Build();
+
+var app = builder.Build();
+var chatClient = app.Services.GetRequiredService<IChatClient>();
+
+var chatCompletion = await chatClient.CompleteAsync("How can you help me?");
+Console.WriteLine(chatCompletion.Message.Text);
+
+
 Console.ReadKey();
