e534cbf849d18646597ddf94bc37e2082670d8c2
Ebicoglu <alper.ebicoglu@volosoft.com>
2025-02-06 13:16:44 +0300

    refactor

--- PhotoPicker/Program.cs
+++ PhotoPicker/Program.cs
@@ -2,15 +2,14 @@
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenAI;
 
 ////////   1.) BUILD THE DEPENDENCY INJECTION   //////// 
 var builder = Host.CreateApplicationBuilder();
-var chatClient = builder.Services.AddChatClient(new OpenAIChatClient(new OpenAIClient(Environment.GetEnvironmentVariable("OPENAI_API_KEY")), "gpt-4o")).Build();
-//var chatClient = builder.Services.AddChatClient(new OllamaChatClient(endpoint: new Uri("http://localhost:11434"), modelId: "llama3.2-vision")).Build();
+//var chatClient = builder.Services.AddChatClient(new OpenAIChatClient(new OpenAIClient(Environment.GetEnvironmentVariable("OPENAI_API_KEY")), "gpt-4o")).Build();
+var chatClient = builder.Services.AddChatClient(new OllamaChatClient(endpoint: new Uri("http://localhost:11434"), modelId: "llama3.2-vision")).Build();
 
 
-////////   2.) ASK TO AI   ////////
+////////   2.) CREATE THE PROMPTS    ////////
 var images = new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\photos")).GetFiles("*.jpg");
 var aiResponses = new List<AiResponse>(images.Length);
 foreach (var image in images)
@@ -25,8 +24,8 @@ foreach (var image in images)
                     Analyze images for Instagram aesthetics on a scale from 1 to 100, considering composition, lighting, and engagement potential.
                     Always return a **valid, fully enclosed JSON object** in the exact format:
                     {         
-                      'rating': <number between 1 and 100>,
-                      'reason': '<Brief explanation of why the rating was given>'
+                      ""rating"": <number between 1 and 100>,
+                      ""reason"": ""<Brief explanation of why the rating was given>""
                     }
 
                     ### **Rules:**
@@ -37,43 +36,69 @@ foreach (var image in images)
 
                     Example output:
                     {         
-                        'rating': 85,
-                        'reason': 'The image has strong composition and lighting.'
+                        ""rating"": 85,
+                        ""reason"": ""The image has strong composition and lighting.""
                     }
                "
         },
         new ChatMessage
         {
             Role = ChatRole.User,
-            Text =  @"Analyze this image:",
+            Text =  "Analyze this image",
             Contents = new List<AIContent> { new ImageContent(File.ReadAllBytes(image.FullName), "image/jpeg") }
         }
     };
 
-    Console.WriteLine("» Analyzing: " + image.Name);
 
+    ////////   3.) ASK TO AI   ////////
+    Console.WriteLine("» ASK TO AI: " + image.Name);
     var chatResponse = await chatClient.CompleteAsync(messages);
-    var jsonResult = chatResponse.Message.Text.Trim();
-    jsonResult = jsonResult.TrimStart("\"```json\"".ToCharArray()).TrimEnd("```".ToCharArray()).Trim();
+    Console.WriteLine("» AI-RESPONSE: \n" + chatResponse.Message.Text + "\n" + new string('═', 100));
 
-    Console.WriteLine("» AI-RESPONSE: \n" + jsonResult + "\n" + new string('═', 100));
 
-    var aiResponse = JsonSerializer.Deserialize<AiResponse>(jsonResult, new JsonSerializerOptions
+    ////////   4.) DESERIALIZE THE AI RESPONSE   ////////
+    if (TryParseJson<AiResponse>(chatResponse.Message.Text, out var aiResponse))
     {
-        PropertyNameCaseInsensitive = true,
-        ReadCommentHandling = JsonCommentHandling.Skip,
-        AllowTrailingCommas = true
-    });
-
-    aiResponse.Filename = image.Name;
-    aiResponses.Add(aiResponse);
+        aiResponse.Filename = image.Name;
+        aiResponses.Add(aiResponse);
+    }
 }
 
 
-//////   3.) FIND THE WINNER   ////////
+//////   5.) FIND THE WINNER   ////////
 var winner = aiResponses.MaxBy(x => x.Rating);
 Console.WriteLine("»»» WINNER: " + winner?.Filename + " «««");
 Console.ReadLine();
+
+
+
+bool TryParseJson<T>(string? text, out T? result)
+{
+    try
+    {
+        text = text
+            .Trim()
+            .TrimStart("\"```json\"".ToCharArray())
+            .TrimEnd("```".ToCharArray()).Trim();
+
+        var r = JsonSerializer.Deserialize<dynamic>(text);
+
+        result = JsonSerializer.Deserialize<T>(text, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            ReadCommentHandling = JsonCommentHandling.Skip,
+            AllowTrailingCommas = true
+        });
+
+        return true;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        result = default(T);
+        return false;
+    }
+}
 
 
 internal class AiResponse
@@ -82,3 +107,4 @@ internal class AiResponse
     public byte Rating { get; set; }
     public string Reason { get; set; }
 }
+
